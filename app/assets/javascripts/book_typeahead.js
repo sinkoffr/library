558b7eecd5931bb5eb7c88fc8ae196501ec5a7af
Rebecca <rebecca.sinkoff@gmail.com>
2018-02-08 21:26:34 +0000

    Updated minLength in typeahead

--- app/assets/javascripts/book_typeahead.js
+++ app/assets/javascripts/book_typeahead.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
-    var booksDataSource = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      // prefetch: '../data/films/post_1960.json',
-      remote: {
-        url: '../books/google_api_search/%QUERY.json',
-        wildcard: '%QUERY'
-      }
-    });
-    
-    $('#book_title').typeahead(null, {
-      name: 'best-titles',
-      minLength: 3,
-      display: 'title',
-      source: booksDataSource
-    });
-    
-    $('#book_title').on('typeahead:selected', function(evt, item) {
-        console.log(item);
-        var authors = item.authors.join()
-        var description = item.description 
-        $('#book_author').val(authors)
-        $('#book_description').val(description)
-        // do what you want with the item here
-    })
-})
+  var booksDataSource = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // prefetch: '../data/films/post_1960.json',
+    remote: {
+      url: '../books/google_api_search/%QUERY.json',
+      wildcard: '%QUERY'
+    }
+  });
+  
+  $('#book_title').typeahead(null, {
+    name: 'best-titles',
+    minLength: 3,
+    display: 'title',
+    source: booksDataSource
+  });
+  console.log("Test1");
+  
+  $('#book_title').on('typeahead:selected', function(evt, item) {
+      console.log("test");
+      var authors = item.authors.join();
+      var description = item.description; 
+      $('#book_author').val(authors)
+      $('#book_description').val(description)
+      // do what you want with the item here
+  });
+  console.log("test2");
+});
