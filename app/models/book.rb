b2d3b533cb72ea971f6d037094109ca9e3e7ae8b
Rebecca <rebecca.sinkoff@gmail.com>
2018-05-17 19:53:00 +0000

    Update

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,17 +3,4 @@ class Book < ApplicationRecord
   mount_uploader :cover_art, CoverartUploader
   belongs_to :user
   
-  
-  def new
-      @book = Book.new
-  end
-  
-  def index
-     @book = Book.index
-  end
-  
-  def show
-    @book = Book.where(id: :book_id)
-  end
-  
 end
